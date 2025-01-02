f44a0581d5e1c47e97b7458312d54468ee17ef6e
Abhishek <abhishekmandavi8805@gmail.com>
2025-01-02 21:46:36 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NotesPage from "./pages/NotesPage";
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div id="app">
+      <NotesPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fakeData as notes} from "../assets/fakeData.js";
+import NoteCard from "../components/NoteCard";
+
+
+const NotesPage = () => {
+  return (
+    <div>
+      {notes.map((note) => (
+        <NoteCard note={note} key={note.$id} />
+
+      ))}
+    </div>
+  )
+}
+
+export default NotesPage
